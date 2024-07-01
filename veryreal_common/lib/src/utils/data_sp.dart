5e164d97e4a33c8e6bcb8035b36cc5c02aad75a2
LI YUAN XIN <yuanxintaxon@gmail.com>
2024-07-01 12:55:40 +0800

    files cleaning

--- veryreal_common/lib/src/utils/data_sp.dart
+++ veryreal_common/lib/src/utils/data_sp.dart
@@ -5,41 +5,7 @@ import 'package:uuid/uuid.dart';
 class DataSp {
   static const _loginCertificate = 'loginCertificate';
   static const _loginAccount = 'loginAccount';
-  static const _server = "server";
-  static const _ip = 'ip';
   static const _deviceID = 'deviceID';
-  static const _ignoreUpdate = 'ignoreUpdate';
-  static const _language = "%s_languageKey";
-  static const _excludedCountryCodeForPhoneRegistration =
-      "excludedCountryCodeForPhoneRegistration";
-  static const _groupApplication = "%s_groupApplication";
-  static const _friendApplication = "%s_friendApplication";
-  static const _enabledVibration = 'enabledVibration';
-  static const _enabledRing = 'enabledRing';
-  static const _screenPassword = '%s_screenPassword';
-  static const _enabledBiometric = '%s_enabledBiometric';
-  static const _chatFontSizeFactor = '%s_chatFontSizeFactor';
-  static const _chatBackground = '%s_chatBackground_%s';
-  static const _isAppFirstLaunch = 'isAppFirstLaunch';
-  static const _isFirstLogin = 'isFirstLogin';
-  static const _isAIMode = '%s_isAIMode';
-  static const _isAIModeFirstOn = '%s_isAIModeFirstOn';
-  static const _aiAvatar = "%s_aiAvatar";
-  static const _recordedVoicePrompt = "%s_recordedVoicePrompt";
-  static const _recordedAISoulPromptPartOne = "%s_recordedAISoulPromptPartOne";
-  static const _recordedAISoulPromptPartTwo = "%s_recordedAISoulPromptPartTwo";
-  static const _recordedAISoulPromptPartThree =
-      "%s_recordedAISoulPromptPartThree";
-  static const _voiceCloningLastLeftStep = "%s_voiceCloningLastLeftStep";
-  static const _voiceCloningPreviewModelTaskId =
-      "%s_voiceCloningPreviewModelTaskId";
-  static const _voiceCloningActualModelTaskId =
-      "%s_voiceCloningActualModelTaskId";
-  static const _registerLocation = 'location';
-  static const _worldFilterData = "%s_worldFilterData";
-  static const _ongoingSSO = 'ongoingSSO';
-  static const _isDoNotDisturbMode = '%s_isDoNotDisturbMode';
-  static const _lastIgnoreUpdateVersion = 'lastIgnoreUpdateVersion';
 
   DataSp._();
 
@@ -83,22 +49,6 @@ class DataSp {
     return SpUtil().getObject(_loginAccount);
   }
 
-  static Future<bool>? putServerConfig(Map<String, String> config) {
-    return SpUtil().putObject(_server, config);
-  }
-
-  static Future<Map?> getServerConfig() {
-    return SpUtil().getObject(_server);
-  }
-
-  static Future<bool>? putServerIP(String ip) {
-    return SpUtil().putString(ip, ip);
-  }
-
-  static Future<String?> getServerIP() {
-    return SpUtil().getString(_ip);
-  }
-
   static Future<String> getDeviceID() async {
     String id = await SpUtil().getString(_deviceID) ?? '';
     if (id.isEmpty) {
@@ -106,24 +56,5 @@ class DataSp {
       SpUtil().putString(_deviceID, id);
     }
     return id;
-  }
-
-  static Future<bool>? putExcludedCountryCodeForPhoneRegistration(
-      List<String> countryCodes) {
-    return SpUtil()
-        .putStringList(_excludedCountryCodeForPhoneRegistration, countryCodes);
-  }
-
-  static Future<List<String>?> getExcludedCountryCodeForPhoneRegistration() {
-    return SpUtil()
-        .getStringList(_excludedCountryCodeForPhoneRegistration, defValue: []);
-  }
-
-  static Future<bool>? putRegisterLocation(String location) {
-    return SpUtil().putString(_registerLocation, location);
-  }
-
-  static Future<String?> getRegisterLocation() {
-    return SpUtil().getString(_registerLocation, defValue: null);
   }
 }
